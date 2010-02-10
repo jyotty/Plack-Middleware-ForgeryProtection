791fbad25ad6119aa9c213b3d63cf863f9d42ce3
Joshua Yotty <joshyotty@gmail.com>
2010-02-10 02:40:20 -0800

    ForgeryProtection initial implementation

--- lib/Plack/Middleware/ForgeryProtection.pm
+++ lib/Plack/Middleware/ForgeryProtection.pm
@@ -2,14 +2,32 @@ package Plack::Middleware::ForgeryProtection;
 use strict;
 use warnings;
 use parent qw(Plack::Middleware);
+use Crypt::Random::Source qw(get_strong);
+use MIME::Base64;
+use Plack::Request;
+
 our $VERSION = '0.01';
+
+sub call {
+    my ($self, $env) = @_;
+
+    $env->{'psgix.session'}{'_csrf_token'} ||= encode_base64(get_strong(32), '');
+
+    if ($env->{REQUEST_METHOD} ne 'GET') {
+        my $req = Plack::Request->new($env);
+        if ($req->body_parameters->{'_csrf_token'} ne $env->{'psgix.session'}{'_csrf_token'}) {
+            return [ 403, [ 'Content-Type' => 'text/html; charset=utf-8'], [ '403 Forbidden' ] ];
+        }
+    }
+    return $self->app->($env);
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Plack::Middleware::ForgeryProtection -
+Plack::Middleware::ForgeryProtection - Ronco Spray-On CSRF protection
 
 =head1 SYNOPSIS
 
@@ -17,7 +35,7 @@ Plack::Middleware::ForgeryProtection -
 
 =head1 DESCRIPTION
 
-Plack::Middleware::ForgeryProtection is
+Plack::Middleware::ForgeryProtection is amazing
 
 =head1 AUTHOR
 
