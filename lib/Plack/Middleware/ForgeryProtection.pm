1bde444eeb8c84ef84d5991194efb4d2ed2e1bac
Joshua Yotty <joshyotty@gmail.com>
2010-02-10 01:20:10 -0800

    Initial commit

---
+++ lib/Plack/Middleware/ForgeryProtection.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::ForgeryProtection;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::ForgeryProtection -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::ForgeryProtection;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::ForgeryProtection is
+
+=head1 AUTHOR
+
+Joshua Yotty
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
